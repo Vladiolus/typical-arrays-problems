437ebe3eb812f55969ba902d4b43a4a318f7a72a
Vladiolus <uda4a.vlad@mail.ru>
2020-03-22 14:45:06 +0300

    feat: add solution to task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length == 0){
+    return 0;
+  }
+  let mini = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < mini) mini = array[i];
+  }
+  return mini;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length == 0){
+    return 0;
+  }
+  let maxi = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > maxi) maxi = array[i];
+  }
+  return maxi;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length == 0){
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+  return sum /= array.length;
 }
